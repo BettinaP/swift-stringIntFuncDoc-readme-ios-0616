b9baff6212b9309c6915d2631592d241a9eecdd2
BettinaP <bettinaprophete@gmail.com>
2016-07-05 22:31:47 -0400

    done

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -2,5 +2,5 @@ let name = "Audrey"
 name.uppercaseString
 
 let uppercaseName = name.uppercaseString
-print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
+print("\(name)... \(name)... \(uppercaseName)! WAKE UP!!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
